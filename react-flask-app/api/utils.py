fbc02e3e41da89238b2c71eadc3c250965128858
Ubuntu <ubuntu@ip-172-31-41-235.eu-west-1.compute.internal>
2020-10-05 03:22:15 +0000

    change to utils

--- react-flask-app/api/utils.py
+++ react-flask-app/api/utils.py
@@ -10,13 +10,13 @@ warnings.filterwarnings('ignore')
 
 
 # get the GTFS timetable information
-##timetable_df = pd.read_csv("google_transit_combined/stop_times.txt")
+timetable_df = pd.read_csv("google_transit_combined/parsed_stop_times.txt")
 # get the GTFS trips information
-##trips_df = pd.read_csv("google_transit_combined/trips.txt")
+trips_df = pd.read_csv("google_transit_combined/trips.txt")
 # get the GTFS route information
-##route_df = pd.read_csv("google_transit_combined/routes.txt")
+route_df = pd.read_csv("google_transit_combined/routes.txt")
 # get the GTFS calendar information
-##calendar_df = pd.read_csv("google_transit_combined/calendar.txt")
+calendar_df = pd.read_csv("google_transit_combined/calendar.txt")
 # placeholder object to return to the frontend in the event that no data exists
 placeholder = {'results': [
     {
