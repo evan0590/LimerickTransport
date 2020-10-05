fe7751fbab2a4959ebbf4a5d4331719e2b9ccc98
Evan Byrnes <evan.byrnes@ucdconnect.ie>
2020-10-05 01:30:19 +0100

    test again

--- limk-flask-api/app/settings.py
+++
@@ -1,4 +0,0 @@
-# demo/settings.py
-from os import environ
-
-NTA_API_KEY = environ.get('NTA_API_KEY')
--- limk-flask-api/run.py
+++
@@ -1,8 +0,0 @@
-# run.py
-from app import create_app
-from dotenv import load_dotenv
-
-# the path to your .env file (or any other file of environment variables you want to load)
-load_dotenv('.env')
-
-app = create_app()
